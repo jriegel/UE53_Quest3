9fbd7e810bb0a33957590aef61686140d7445abf
Jürgen Riegel <mail@juergen-riegel.net>
2024-10-15 20:42:51 +0200

    Add code

---
+++ Source/VrQuest/VrBlueprintFunctionLibrary.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "VrBlueprintFunctionLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VRQUEST_API UVrBlueprintFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
